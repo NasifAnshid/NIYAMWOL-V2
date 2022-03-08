17403d68fa664748005f5197311f291200dc8f41
SHIBILY <99062594+SHIBI-SER@users.noreply.github.com>
2022-03-08 16:36:23 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -30,7 +30,7 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*TAURUS BOT BY MUHAMMED*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*SHIBISER BY SHIBI*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n--------------------', MessageType.text);
