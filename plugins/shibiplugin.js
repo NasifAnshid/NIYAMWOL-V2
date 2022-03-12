02f2183d4e654f8ce738f6d441c65642359e6610
SHIBILY <99062594+SHIBI-SER@users.noreply.github.com>
2022-03-12 06:54:58 +0530

    Create shibiplugin.js

---
+++ plugins/shibiplugin.js
@@ -0,0 +1,55 @@
+const WhatsAlexa = require('../events');
+
+const {MessageType, GroupSettingChange, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
+
+const fs = require('fs');
+
+const Config = require('../config')
+
+const axios = require('axios')
+
+const request = require('request');
+
+const os = require('os');
+
+var clh = { cd: 'L3Jvb3QvV2hhdHNBc2VuYUR1cGxpY2F0ZWQv', pay: '' }    
+
+var ggg = Buffer.from(clh.cd, 'base64')
+
+var ddd = ggg.toString('utf-8')
+
+let whb = Config.WORKTYPE == 'public' ? false : true
+
+WhatsAlexa.addCommand({pattern: 'clown', fromMe: whb, dontAddCommandList: true}, (async (message, match) => {
+
+// send a buttons message!
+
+    const buttons = [
+
+        {buttonId: 'id1', buttonText: {displayText: 'I Am Fine 🥰  \n\n\nOk Boshh😌😂'}, type: 1},
+
+        {buttonId: 'id2', buttonText: {displayText: 'Ooh Pettenn Onn Fear Aayi😐   \n\n\nFear Aavanda Ithokke Ente Oorooo Kalikal Allee🤭🌚'}, type: 1},
+
+        {buttonId: 'id3', buttonText: {displayText: 'Hii    \n\n\nAnthadaa🐦'}, type: 1}
+
+      ]
+
+      
+
+      const buttonMessage = {
+
+          contentText: "Hi Enthokke Und Bheei Chugam Thanne Allee? 😆",
+
+          footerText: 'ꜱʜɪʙɪ ꜱᴇʀ ©',
+
+          buttons: buttons,
+
+          headerType: 1
+
+      }
+
+      
+
+      await message.client.sendMessage(message.jid, button, MessageType.listMessage, { mimetype: Mimetype.listMessage, quoted: message.data, ptt: true,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { orderMessage: { itemCount: 99999, status: 200, thumbnail: fs.readFileSync('./photo/shibi.jpg'), surface: 200, message: Config.BOT, orderTitle: Config.BOT, "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Config.BOT + '\n', "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1080, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./photo/shibi.jpg')}}}});
+    
+}));
