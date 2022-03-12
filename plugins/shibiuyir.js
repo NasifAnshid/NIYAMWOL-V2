d77e800a30d0dbd41822fc38fd6e522b441ce0d1
SHIBILY <99062594+SHIBI-SER@users.noreply.github.com>
2022-03-12 06:06:43 +0530

    Create shibiuyir.js

---
+++ plugins/shibiuyir.js
@@ -0,0 +1,25 @@
+/* codded by raashii
+reedited by BASIL SIR
+use this git under copyright
+dont change credit
+*/
+
+const Badboy= require('../events');
+const config = require('../config');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+const hrs = new Date().getHours({ timeZone: 'Asia/Kolkata' })
+
+Badboy.addCommand({pattern: 'menu', fromMe: false, desc: 'it send bot menu'}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    *edaa eda manda idh shibi ahada thayathilla*
+│
+│ 
+│
+│   ❏🎀ꜱʜɪʙɪ sɪʀ🎀❏
+╰──────────────────╯
+`}) 
+
+}));
